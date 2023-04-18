497f043bd2aafe7b9ead0d33ec42e95d9a5c990f
Larissa Ramos <devlarissasr@gmail.com>
2023-04-18 18:05:58 -0300

    :package: react-icons

---
+++ web/src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import ButtonMode from "@/components/ButtonMode";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <div>
+        <ButtonMode/>
+      </div>
+    </div>
+  );
+}
