d9b8bd4104ef4714ae5f70444738193d0fbb9132
Larissa Ramos <devlarissasr@gmail.com>
2023-04-24 16:55:34 -0300

    :bug:

--- web/src/pages/api/coupons.ts
+++
@@ -1,7 +0,0 @@
-import prisma from '../../lib/prisma';
-
-export default async function handler(req: any, res: any) {
-  const users = await prisma.user.findMany();
-
-  res.status(200).json(users);
-}
