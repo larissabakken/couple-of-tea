969be4bb0fe3d5a7b01cc5991c1f4ce51ce61481
Larissa Ramos <devlarissasr@gmail.com>
2023-04-29 12:53:54 -0300

    :bug:

--- web/src/components/invitation/index.tsx
+++ web/src/components/invitation/index.tsx
@@ -104,9 +104,7 @@ const Invitation: React.FC<InvitationProps> = ({
                   </div>
 
                   <div className="bg-gray-50 px-4 py-5 sm:px-6">
-                    <iframe
-                      src={weddingMapLink}
-                    ></iframe>
+                    <iframe src={weddingMapLink}></iframe>
                   </div>
                 </dl>
               </div>
@@ -130,6 +128,22 @@ const Invitation: React.FC<InvitationProps> = ({
               {isOpenMeal && (
                 <div className="border-t border-gray-200">
                   <dl>
+                    <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6">
+                      <dt className="text-sm font-medium text-gray-500">
+                        {language === "pt-BR"
+                          ? "Data da celebração"
+                          : language === "en-US"
+                          ? "Celebration Date"
+                          : "Feiring Dato"}
+                      </dt>
+                      <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-1">
+                        {
+                          language === "pt-BR" ? "29 de Julho as 14:00, será após o casamento" :
+                          language === "en-US" ? "July 29th at 2:00 PM, will be after the wedding" :
+                          "29. juli kl 14:00, vil være etter bryllupet"
+                        }
+                      </dd>
+                    </div>
                     <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6">
                       <dt className="text-sm font-medium text-gray-500">
                         {language === "pt-BR"
