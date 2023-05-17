8d379eeac7e972a8036e71944dc383fc4f571640
Larissa Ramos <devlarissasr@gmail.com>
2023-05-17 14:48:30 -0300

    :construction: add shelf and whislist

---
+++ src/pages/store-wishlist/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from "@/components/header";
+import Shelf from "@/components/shelf";
+export default function StoreWishlist() {
+  return (
+    <div>
+      <Header/>
+      <Shelf/>
+    </div>
+  )
+}
