8ab4bfedb2f2238d804649d8cbb7a62d7b689935
Larissa Bakken <devlarissasr@gmail.com>
2023-07-05 12:33:07 +0200

    add react hook form

--- src/pages/store-wishlist/form/[slug].tsx
+++ src/pages/store-wishlist/form/[slug].tsx
@@ -3,10 +3,17 @@ import Link from 'next/link'
 import ptTranslations from '@/locales/br.json'
 import noTranslations from '@/locales/no.json'
 import { useRouter } from 'next/router'
+import { useForm, SubmitHandler } from 'react-hook-form'
 
 export default function Form() {
   const router = useRouter()
   const language = router.query.slug as string
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<any>()
 
   const translations: any = {
     'pt-BR': ptTranslations,
@@ -16,22 +23,23 @@ export default function Form() {
   const t = translations[language]
 
   const handleEmail = async () => {
-    try {
-      const response = await fetch('/api/email', { method: 'POST' })
-      if (response.ok) {
-        console.log('Email sent successfully')
-      } else {
-        console.error('Failed to send email:', response.status)
-      }
-    } catch (error) {
-      console.error('Failed to send email:', error)
-    }
+    // try {
+    //   const response = await fetch('/api/email', { method: 'POST' })
+    //   if (response.ok) {
+    //     console.log('Email sent successfully')
+    //   } else {
+    //     console.error('Failed to send email:', response.status)
+    //   }
+    // } catch (error) {
+    //   console.error('Failed to send email:', error)
+    // }
   }
+  const onSubmit: SubmitHandler<any> = (data) => console.log(data)
 
   return (
     <div className="w-full min-h-[90vh] flex items-center justify-center">
       <div className="min-w-[5rem] mx-10">
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="border-b border-gray-900/10 py-12">
             <h2 className="text-base font-semibold leading-7 text-gray-900">
               {t.store.form.title}
@@ -42,55 +50,38 @@ export default function Form() {
 
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <div className="sm:col-span-4">
-                <label
-                  htmlFor="first-name"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
+                <label className="block text-sm font-medium leading-6 text-gray-900">
                   {t.store.form.name}
                 </label>
                 <div className="mt-2">
                   <input
-                    type="text"
-                    name="first-name"
-                    id="first-name"
-                    autoComplete="given-name"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[var(--primary-color-dark)] sm:text-sm sm:leading-6"
+                    {...register('name', { required: true })}
+                    className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[var(--primary-color-dark)] sm:text-sm sm:leading-6"
                   />
+                  {errors.name && <span>This field is required</span>}
                 </div>
               </div>
 
               <div className="sm:col-span-3">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
+                <label className="block text-sm font-medium leading-6 text-gray-900">
                   {t.store.form.email}
                 </label>
                 <div className="mt-2">
                   <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[var(--primary-color-dark)] sm:text-sm sm:leading-6"
+                    {...register('email')}
+                    className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[var(--primary-color-dark)] sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
 
               <div className="sm:col-span-3">
-                <label
-                  htmlFor="postal-code"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
+                <label className="block text-sm font-medium leading-6 text-gray-900">
                   {t.store.form.phone}
                 </label>
                 <div className="mt-2">
                   <input
-                    type="text"
-                    name="phone"
-                    id="phone"
-                    autoComplete="phone"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[var(--primary-color-dark)] sm:text-sm sm:leading-6"
+                    {...register('phone')}
+                    className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[var(--primary-color-dark)] sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
@@ -108,11 +99,10 @@ export default function Form() {
                 </p>
                 <div className="mt-2">
                   <textarea
-                    id="about"
-                    name="about"
+                    {...register('cart')}
+                    defaultValue={'...'}
                     rows={3}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[var(--primary-color-dark)] sm:text-sm sm:leading-6"
-                    defaultValue={''}
+                    className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[var(--primary-color-dark)] sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
@@ -123,18 +113,16 @@ export default function Form() {
             <div className="my-4 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <div className="col-span-full">
                 <h2 className="text-base font-semibold leading-7 text-gray-900">
-                  Mensagem
+                  {t.store.form.message}
                 </h2>
                 <p className="mt-3 text-sm leading-6 text-gray-600">
-                  Se você quiser nos escrever um cartão presente, escreva aqui!
-                  👇
+                  {t.store.form.messageDescription}
                 </p>
                 <div className="mt-2">
                   <textarea
-                    id="about"
-                    name="about"
-                    rows={3}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[var(--primary-color-dark)] sm:text-sm sm:leading-6"
+                    {...register('message')}
+                    rows={8}
+                    className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[var(--primary-color-dark)] sm:text-sm sm:leading-6"
                     defaultValue={''}
                   />
                 </div>
@@ -150,53 +138,40 @@ export default function Form() {
               {t.store.form.payDescription}
             </p>
 
-            <div className="mt-2 space-y-10">
-              <fieldset>
-                <div className="mt-6 space-y-6">
-                  <div className="flex items-center gap-x-3">
-                    <input
-                      id="bank"
-                      name="payment"
-                      type="radio"
-                      className="h-4 w-4 border-gray-300 text-[var(--primary-color-dark)] focus:ring-[var(--primary-color-dark)]"
-                    />
-                    <label
-                      htmlFor="bank"
-                      className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                      {t.store.form.payOptions.option1}
-                    </label>
-                  </div>
-                  <div className="flex items-center gap-x-3">
-                    <input
-                      id="delivery"
-                      name="payment"
-                      type="radio"
-                      className="h-4 w-4 border-gray-300 text-[var(--primary-color-dark)] focus:ring-[var(--primary-color-dark)]"
-                    />
-                    <label
-                      htmlFor="delivery"
-                      className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                      {t.store.form.payOptions.option2}
-                    </label>
-                  </div>
-                  <div className="flex items-center gap-x-3">
-                    <input
-                      id="nothing"
-                      name="payment"
-                      type="radio"
-                      className="h-4 w-4 border-gray-300 text-[var(--primary-color-dark)] focus:ring-[var(--primary-color-dark)]"
-                    />
-                    <label
-                      htmlFor="nothing"
-                      className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                      {t.store.form.payOptions.option3}
-                    </label>
-                  </div>
-                </div>
-              </fieldset>
+            <div className="mt-4 space-y-4">
+              <div>
+                <label className="block text-sm font-medium leading-6 text-gray-900">
+                  <input
+                    className="h-4 w-4 border-gray-300 text-[var(--primary-color-dark)] focus:ring-[var(--primary-color-dark)] mr-4"
+                    type="radio"
+                    value={t.store.form.payOptions.option1}
+                    {...register('payOption')}
+                  />
+                  {t.store.form.payOptions.option1}
+                </label>
+              </div>
+              <div>
+                <label className="block text-sm font-medium leading-6 text-gray-900">
+                  <input
+                    className="h-4 w-4 border-gray-300 text-[var(--primary-color-dark)] focus:ring-[var(--primary-color-dark)] mr-4"
+                    type="radio"
+                    value={t.store.form.payOptions.option2}
+                    {...register('payOption')}
+                  />
+                  {t.store.form.payOptions.option2}
+                </label>
+              </div>
+              <div>
+                <label className="block text-sm font-medium leading-6 text-gray-900">
+                  <input
+                    className="h-4 w-4 border-gray-300 text-[var(--primary-color-dark)] focus:ring-[var(--primary-color-dark)] mr-4"
+                    type="radio"
+                    value={t.store.form.payOptions.option3}
+                    {...register('payOption')}
+                  />
+                  {t.store.form.payOptions.option3}
+                </label>
+              </div>
             </div>
           </div>
 
