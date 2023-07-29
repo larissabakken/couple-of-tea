5516789afcead7b314c01a0b7fcc26d3650c9bf9
Larissa Bakken <devlarissasr@gmail.com>
2023-07-29 06:47:13 +0200

    add link

--- src/components/countdownWedding/index.tsx
+++ src/components/countdownWedding/index.tsx
@@ -11,7 +11,7 @@ const Countdown: React.FC<CountdownProps> = ({ language }) => {
 
   useEffect(() => {
     const countDownDate =
-      new Date('July 29, 2023 11:00:00 GMT+0100').getTime() + 60 * 60 * 1000
+      new Date('July 29, 2023 09:00:00 GMT+0100').getTime() + 60 * 60 * 1000
 
     const interval = setInterval(() => {
       const now = new Date().getTime()
@@ -42,16 +42,16 @@ const Countdown: React.FC<CountdownProps> = ({ language }) => {
         {days}d {hours}h {minutes}m {seconds}s
       </div>
       <Link
-        href="/wedding"
+        href="https://www.twitch.tv/pandinhaow"
         target="_blank"
         rel="noopener noreferrer"
         className="bg-[var(--primary-color-dark)] hover:bg-[var(--primary-color)] text-white font-bold py-2 px-4 rounded cursor-pointer"
       >
         {language === 'pt-BR'
-          ? 'Assista ao Vivo'
+          ? 'Assista ao Vivo na Twitch as 06:00 AM'
           : language === 'en-US'
-          ? 'Join the Live Stream'
-          : 'Se Live Stream'}
+          ? 'Join the Live Stream on Twitch at 11:00 AM'
+          : 'Se Live Stream på Twitch - 11:00'}
       </Link>
       <div className="text-sm text-[var(--error)] mt-4 max-w-[800px] text-center px-4">
         {language === 'pt-BR'
@@ -60,6 +60,13 @@ const Countdown: React.FC<CountdownProps> = ({ language }) => {
           ? "The link will be available 5 minutes before the ceremony starts.Don't worry, the ceremony lasts around 10 minutes."
           : 'Lenken vil være tilgjengelig 5 minutter før seremonien starter.Ikke bekymre deg, seremonien varer rundt 10 minutter.'}
         *
+      </div>
+      <div className="text-sm text-[var(--error)] mt-4 max-w-[800px] text-center px-4">
+        {language === 'pt-BR'
+          ? 'Você será direcionado para o meu canal na twitch, a transmissão ficara disponível nesse canal assim que começar então no se preocupe é so aguardar na pagina que você será direcionado!'
+          : language === 'en-US'
+          ? "You will be directed to my Twitch channel, the broadcast will be available on this channel as soon as it starts so don't worry, just wait on the page you will be directed to!"
+          : 'Du vil bli sendt til min Twitch-kanal, sendingen vil være tilgjengelig på denne kanalen så snart den begynner, så ikke bekymre deg, bare vent på siden du vil bli sendt til!'}
       </div>
     </div>
   )
